83ee2ac52bc915bf3c8325b810e594aecf8b179e
Matheus Almeida <matheusalmeida016@gmail.com>
2021-07-26 09:17:20 -0400

    Add setup

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pokemon App</title>
+        <meta name="description" content="1ts Generation Pokémon List" />
+      </Head>
+
+      <main>
+        <h1>Hello, World!</h1>
+      </main>
+  )
+}
