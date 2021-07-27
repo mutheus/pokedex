3383aee6bae12e09ea54a6228688a935368841d1
Matheus Almeida <matheusalmeida016@gmail.com>
2021-07-26 21:44:58 -0400

    Add font family

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <title>Pokemon App</title>
+          <meta name="description" content="1ts Generation Pokémon List" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Hind:wght@400;500&family=Inconsolata&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
-import Head from 'next/head'
 import styles from './styles.module.scss'
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Pokemon App</title>
-        <meta name="description" content="1ts Generation Pokémon List" />
-      </Head>
-
       <main>
         <h1>Hello, World!</h1>
       </main>
